e31a02df79801984bdd00f636ab7745e5fed483e
baren maulana <barenmaulana@gmail.com>
2023-08-11 11:29:32 +0700

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,17 +39,27 @@ export default {
     },
   },
 
-  build: [
+  builds: [
     {
-      src: "src/index.js",
-      use: "@vercel/node",
+      src: "nuxt.config.js",
+      use: "@nuxtjs/now-builder",
+      config: {
+        serverFiles: ["package.json"],
+      },
     },
   ],
+
   routes: [
+    { src: "/_nuxt/.+", headers: { "Cache-Control": "max-age=31557600" } },
     {
-      src: "/(.*)",
-      dest: "src/index.js",
+      src: "/sw.js",
+      dest: "/_nuxt/static/sw.js",
+      headers: {
+        "cache-control": "public, max-age=43200, immutable",
+        "Service-Worker-Allowed": "/",
+      },
     },
+    { src: "/(.*)", dest: "/" },
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
